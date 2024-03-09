79592e072298a63e1e8d410867eaaa3e07719ae8
unknown <abcdmark25@yahoo.com>
2024-03-09 00:02:49 +0000

    login UI

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.scss'
+import Dashboard from './components/Dashboard/Dashboard'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+
+//Import routes
+import{
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+//create route
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div> <Login /> </div>
+  },
+  {
+    path: '/register',
+    element: <div> <Register /> </div>
+  },
+  {
+    path: '/dashboard',
+    element: <div> <Dashboard /> </div>
+  }
+])
+
+function App() {
+  
+
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard Page 
+      <a href="/">Logout</a>
+    </div>
+  )
+}
+
+export default Dashboard
